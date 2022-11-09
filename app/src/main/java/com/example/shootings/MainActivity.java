0d8ec80b34ac2cb12a6e9f368d4cf40251f83179
kato-love-cat <kato.lovecat@gmail.com>
2022-11-10 01:50:57 +0900

    2022/11/10 1:50

--- app/src/main/java/com/example/shootings/MainActivity.java
+++ app/src/main/java/com/example/shootings/MainActivity.java
@@ -2,10 +2,17 @@ package com.example.shootings;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +25,23 @@ public class MainActivity extends AppCompatActivity {
     private ImageView enemy_ship;
     private ImageView ship;
 
+    private int frameHeight;
+    private int screenWidth;
+    private int screenHeight;
+
     private float shipY;
     private float enemy_shipY;
+    private float blueballX;
+    private float blueballY;
+    private float greenballX;
+    private float greenballY;
+    private float redballX;
+    private float redballY;
+    private float yellowballX;
+    private float yellowballY;
+
+    private Handler handler = new Handler();
+    private Timer timer = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +57,14 @@ public class MainActivity extends AppCompatActivity {
         redball = findViewById(R.id.redball);
         yellowball = findViewById(R.id.yellowball);
 
+        // Screen Size
+        WindowManager wm = getWindowManager();
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        screenWidth = size.x;
+        screenHeight = size.y;
         blueball.setX(-80.0f);
         blueball.setY(-80.0f);
         greenball.setX(-80.0f);
@@ -47,5 +77,48 @@ public class MainActivity extends AppCompatActivity {
         startLabel.setVisibility(View.INVISIBLE);
         shipY = 500.0f;
 
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        changePos();
+                    }
+                });
+            }
+        }, 0, 20);
     }
+
+    public void changePos() {
+
+        // greenball
+        greenballY += 20;
+        if (greenballY > screenHeight) {
+            greenballY = -80.0f;
+            greenballX = (float)Math.floor(Math.random() * (screenWidth - greenball.getWidth()));
+        }
+        greenball.setX(greenballX);
+        greenball.setY(greenballY);
+
+        // redball
+        redballY += 40;
+        if (redballY > screenHeight) {
+            redballY = -80.0f;
+            redballX = (float)Math.floor(Math.random() * (screenWidth - redball.getWidth()));
+        }
+        redball.setX(redballX);
+        redball.setY(redballY);
+
+        // yellowball
+        yellowballY += 30;
+        if (yellowballY > screenHeight) {
+            yellowballY = -80.0f;
+            yellowballX = (float)Math.floor(Math.random() * (screenWidth - yellowball.getWidth()));
+        }
+        yellowball.setX(yellowballX);
+        yellowball.setY(yellowballY);
+
+    }
+
 }
